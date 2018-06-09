8a4cc940a3b1f527906aa8ea95157a19cd333158
晓波 郭 <guoxiaobo96@gmail.com>
2018-06-09 11:54:28 +0800

    test

---
+++ cnn_model.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+import pickle
+
+
+class TCNNConfig(object):
+    """CNN配置参数"""
+    vocab_dim = 400
+    embedding_size = 128  # 词向量维度
+    seq_length = 300  # 序列长度
+    num_classes = 5  # 类别数
+    num_filters = 256  # 卷积核数目
+    filter_size = [2, 3, 4, 5]  # 卷积核尺寸
+    vocab_size = 5000  # 词汇表大小
+
+    hidden_dim = 1024  # 全连接层神经元
+
+    learning_rate = 1e-5  # 学习率
+
+    batch_size = 64  # 每批训练大小
+    num_epochs = 10000  # 总迭代轮次
+    kernel_size = 5
+    print_per_batch = 100  # 每多少轮输出一次结果
+    save_per_batch = 10  # 每多少轮存入tensorboard
+    l2_lambda = 0.1
+
+
+class CharLevelCNN(object):
+    """文本分类，CNN模型"""
+
+    def __init__(self, config):
+        self.config = config
+        # 三个待输入的数据
+        self.x = tf.placeholder(
+            tf.int32, [None, self.config.seq_length], name='x')
+        self.y = tf.placeholder(
+            tf.float32, [None, self.config.num_classes], name='y')
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        self.l2_loss = 0
+        self.l2_lambda = 0.2
+
+        self.char_level_cnn()
+
+    def char_level_cnn(self):
+        """CNN模型"""
+        # 词向量映射
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable(
+                'embedding', [self.config.vocab_size, self.config.embedding_size])
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.x)
+
+        with tf.name_scope("cnn"):
+            # CNN layer
+            conv = tf.layers.conv1d(
+                embedding_inputs, self.config.num_filters, self.config.kernel_size, name='conv')
+            # global max pooling layer
+            gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
+            fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
+            fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+            W = tf.get_variable("W", shape=[
+                                self.config.hidden_dim, self.config.num_classes], initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(
+                0.1, shape=[self.config.num_classes]), name="b")
+            self.l2_loss += tf.nn.l2_loss(W)
+            self.l2_loss += tf.nn.l2_loss(b)
+            self.logits = tf.nn.xw_plus_b(fc, W, b)
+            self.y_pred = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
+
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+                logits=self.logits, labels=self.y)
+            self.loss = tf.reduce_mean(
+                cross_entropy)+self.l2_loss*self.l2_lambda
+            # 优化器
+            self.optimizer = tf.train.AdamOptimizer(
+                learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            # 准确率
+            correct_pred = tf.equal(tf.argmax(self.y, 1), self.y_pred)
+            self.precision = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
---
+++ prepareData.py
@@ -0,0 +1,152 @@
+def read_data(file_name, target_case="term_of_imprisonment"):
+    import json
+    contents, labels = [], []
+    with open(file=file_name, mode='r', encoding='utf8') as f:
+        for line in f:
+            content = json.loads(line)["fact"]
+            label = json.loads(line)["meta"][target_case]
+            if label["death_penalty"]:
+                label = -2
+            elif label["life_imprisonment"]:
+                label = -1
+            else:
+                label = label["imprisonment"]
+            if content:
+                contents.append(content)
+                labels.append(label)
+    return contents, labels
+
+
+def build_vocab(train_dir, valid_dir, test_dir, vocab_dir, vocab_size, min_frequence):
+    from collections import Counter
+    contents, _ = read_data(test_dir)
+    temp, _ = read_data(valid_dir)
+    contents += temp
+    temp, _ = read_data(train_dir)
+    contents += temp
+
+    all_data = []
+    for content in contents:
+        all_data.extend(content)
+
+    counter = Counter(all_data)
+    count_pairs = counter.most_common(vocab_size-1)
+    vocab_list = list(zip(*count_pairs))
+    index = vocab_list[1].index(min_frequence)
+    words = vocab_list[0]
+    # 添加一个 <PAD> 来将所有文本pad为同一长度
+    words = ['<PAD>'] + list(words)[:index]
+    with open(vocab_dir, mode='w', encoding='utf8') as f:
+        f.write('\n'.join(words) + '\n')
+
+
+def read_vocab(vocab_dir):
+    with open(vocab_dir, mode='r', encoding='utf8') as fp:
+        words = [_.strip() for _ in fp.readlines()]
+        word_to_id = dict(zip(words, range(len(words))))
+    return words, word_to_id
+
+
+def read_word2vec(vocab_dir):
+    with open(vocab_dir, mode='r', encoding='utf8') as fp:
+        vocab_length, vocab_dim = 0, 0
+        words, vectors = [], []
+        for line in fp.readlines():
+            if vocab_length == 0:
+                vocab_length, vocab_dim = line.strip().split(' ')
+            else:
+                words.append(line.strip().split(' ')[0])
+                vectors.append(line.strip().split(' ')[1:])
+        word_to_id = dict(zip(words, vectors))
+    return words, word_to_id, vocab_length, vocab_dim
+
+
+def batch_iter(x, y, batch_size, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    import numpy as np
+    data_size = len(x)
+    num_batches_per_epoch = int((data_size-1)/batch_size) + 1
+    # Shuffle the data at each epoch
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        x_shuffle = x[shuffle_indices]
+        y_shuffle = y[shuffle_indices]
+    else:
+        x_shuffle = x
+        y_shuffle = y
+    for batch_num in range(num_batches_per_epoch):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        yield x_shuffle[start_index:end_index], y_shuffle[start_index:end_index]
+
+
+def get_data_with_vocab(data_dir, words_to_id, cat_to_id, vocab_length):
+    import keras
+    import random
+    contents, labels = read_data(data_dir)
+    data_id, label_id, all_data = [], [], []
+    for i in range(len(contents)):
+        all_data.append(contents[i]+','+labels[i])
+    data_size = len(all_data)
+    random.shuffle(all_data)
+    contents, labels = [], []
+    for _, data in enumerate(all_data):
+        content, label = str(data).strip().split(',')
+        contents.append(content)
+        labels.append(label)
+    for i in range(len(contents)):
+        data_id.append([words_to_id[x]
+                        for x in contents[i] if x in words_to_id])
+        label_id.append(labels[i])
+    x_data = keras.preprocessing.sequence.pad_sequences(
+        data_id, int(vocab_length), dtype='float32')
+    y_data = keras.utils.to_categorical(label_id)
+    return x_data, y_data, all_data
+
+
+def to_words(content, words):
+    return ' '.join(words[x] for x in content)
+
+
+def read_catagory():
+    catagories = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    catagories = [x for x in catagories]
+    cat_to_id = dict(zip(catagories, range(len(catagories))))
+    return catagories, cat_to_id
+
+
+def balance_data(base_dir):
+    import random
+    file = 'felony'
+    file_name = base_dir+file+'_train.txt'
+    contents, labels = read_data(file_name)
+    count = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    all_data, balance_data = [], []
+    for i, label in enumerate(labels):
+        count[int(label)-10] += 1
+        all_data.append(contents[i]+','+labels[i])
+    balance = min(count)
+    count = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    random.shuffle(all_data)
+    random.shuffle(all_data)
+    random.shuffle(all_data)
+    for _, label in enumerate(all_data):
+        content, number = label.split(',')
+        number = int(number)
+        if count[number-10] < balance and len(content) > 30:
+            count[number-10] += 1
+            balance_data.append(label)
+    with open(file=base_dir+file+'_balance.txt', mode='w', encoding='utf8') as f:
+        for i, label in enumerate(balance_data):
+            f.write(label+'\n')
+
+
+def main():
+    build_vocab('D:/cail/good/data_train.json', 'D:/cail/good/data_valid.json', 'D:/cail/good/data_test.json',
+                'D:/cail/good/vocab.txt', vocab_size=5000, min_frequence=10)
+
+
+if __name__ == '__main__':
+    main()
